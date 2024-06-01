dc52a67750f65b5d0c7b430f5e13b9f96b4efa12
autumn <1060590974@qq.com>
2024-06-01 23:00:58 +0800

    1

---
+++ src/exportBatchAsync.cpp
@@ -0,0 +1,94 @@
+#include <opencv2/opencv.hpp>
+#include "infer.hpp"
+#include "cpm.hpp"
+#include "yolo.hpp"
+#include "Config.cpp"
+
+using namespace std;
+
+static cpm::Instance<yolo::BoxArray, yolo::Image, yolo::Infer> cpmi;
+
+static yolo::Image cvimg(const cv::Mat &image) { return yolo::Image(image.data, image.cols, image.rows); }
+
+bool initBatchAsync(const string &engine_file, const float &confidence, const float &nms, const int &batch) {
+    bool ok = cpmi.start([&engine_file, &confidence, &nms] {
+        return yolo::load(engine_file, yolo::Type::V8, confidence, nms);
+    }, batch);
+    if (!ok) {
+        cout << "================================= TensorRT INIT FAIL =================================" << endl;
+        return false;
+    } else {
+        cout << "================================= TensorRT INIT SUCCESS =================================" << endl;
+        return true;
+    }
+}
+
+vector<yolo::Box> inferBatchAsync(vector<cv::Mat> &images) {
+    trt::Timer timer;
+    vector<yolo::Image> yoloimages(images.size());
+    transform(images.begin(), images.end(), yoloimages.begin(), cvimg);
+    timer.start();
+    auto objs = cpmi.commits(yoloimages).back().get();
+    timer.stop("batch 1");
+    return objs;
+}
+
+int main() {
+    Config config;
+    initBatchAsync(config.MODEL, 0.45, 0.5, 3);
+
+    vector<cv::Mat> images{
+            cv::imread(config.TEST_IMG),
+            cv::imread(config.TEST_IMG),
+            cv::imread(config.TEST_IMG)
+    };
+
+    vector<yolo::Box> list = inferBatchAsync(images);
+
+
+    for (const auto &obj: list) {
+        auto name = obj.class_label;
+        cout << "class_label: " << name << " caption: " << obj.confidence << " (L T R B): (" << obj.left << ", "
+             << obj.top << ", " << obj.right << ", " << obj.bottom << ")" <<
+             endl;
+    }
+}
+
+//extern "C" __declspec(dllexport) bool
+//TensorRT_INIT_ASYNC(const char *engine_file, const float confidence, const float nms, const int batch) {
+//    return initBatchAsync(engine_file, confidence, nms, batch);
+//}
+
+//static std::vector<yolo::Box> g_boxes;
+
+//extern "C" __declspec(dllexport) int
+//TensorRT_INFER_NUM_ASYNC(uchar *image, int width, int height) {
+//    g_boxes = inferBatchAsync(image, width, height);
+//    return g_boxes.size();
+//}
+//
+//extern "C" __declspec(dllexport) int
+//GET_LISTBOX_DATA(int boxLabel,
+//                 float *left,
+//                 float *top,
+//                 float *right,
+//                 float *bottom,
+//                 float *confidence,
+//                 int *classLabel) {
+//    if (boxLabel < 0 || boxLabel >= g_boxes.size()) {
+//        return -1;
+//    }
+//    const yolo::Box &box = g_boxes[boxLabel];
+//    *left = box.left;
+//    *top = box.top;
+//    *right = box.right;
+//    *bottom = box.bottom;
+//    *confidence = box.confidence;
+//    *classLabel = box.class_label;
+//    return 0;
+//}
+//
+//extern "C" __declspec(dllexport) void
+//END_GET_LISTBOX_DATA() {
+//    g_boxes.clear();
+//}
