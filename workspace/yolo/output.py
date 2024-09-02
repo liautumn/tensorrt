9651996baf208e2a06505bb909cae644962c19da
autumn <1060590974@qq.com>
2024-09-02 11:21:51 +0800

    1

--- workspace/yolo/output.py
+++ workspace/yolo/output.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO(r"D:\autumn\Documents\JetBrainsProjects\CLionProjects\tensorrt\workspace\yolo\runs\detect\train3\weights\best.pt")  # load an official model
+    model = YOLO(r"D:\autumn\Documents\JetBrainsProjects\CLionProjects\tensorrt\workspace\yolo\yolov8s.pt")  # load an official model
 
     # Export the model
     # ONNX ===> imgsz(h,w), half, dynamic, simplify, opset, batch
