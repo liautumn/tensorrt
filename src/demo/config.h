6924278017447012133abcb0c157f0d5c82d6488
autumn <1060590974@qq.com>
2024-11-14 13:10:03 +0800

    1

--- src/demo/config.h
+++ src/demo/config.h
@@ -7,13 +7,13 @@ using namespace std;
 
 class Config {
 public:
-    const string MODEL = R"(D:\autumn\Documents\JetBrainsProjects\CLionProjects\tensorrt\workspace\model\engine\best.transd.engine)";
+    const string MODEL = R"(D:\autumn\Documents\JetBrainsProjects\CLionProjects\tensorrt\workspace\model\engine\best.engine)";
     const string TEST_IMG = R"(D:\autumn\Documents\WeChat Files\wxid_1w2acnt5bx6s22\FileStorage\File\2024-10\10\d_154_2024-10-14-09-30-00_c10.jpg)";
     const string TEST_IMG_DIRECTORY = R"(D:\autumn\Documents\WeChat Files\wxid_1w2acnt5bx6s22\FileStorage\File\2024-10\10)";
 
-    const char *labels[22] = {
+    const char *labels[23] = {
             "BB", "ZH", "ZK", "JK", "ZZ", "GS", "ZW", "DJ", "PD", "CS", "DW", "HN",
-            "YW", "FH", "LZ", "SYQ", "BQ", "DPD", "MD", "CH", "SD", "SZ"
+            "YW", "FH", "LZ", "SYQ", "BQ", "DPD", "MD", "CH", "SD", "SZ", "ZS"
     };
 
 };
