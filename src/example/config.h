e0feab3fbe40ed0c9ba1071fc081ff661c5b840d
autumn <1060590974@qq.com>
2025-04-06 15:32:05 +0800

    1.0.5

--- src/example/config.h
+++ src/example/config.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class Config {
 public:
-    const string MODEL = R"(D:\autumn\Documents\GitHub\tensorrt\workspace\model\engine\yolo11s.transd.engine)";
+    const string MODEL = R"(D:\autumn\Documents\GitHub\tensorrt\workspace\model\engine\yolo11n.transd.engine)";
     const string TEST_IMG = R"(D:\autumn\Documents\GitHub\tensorrt\workspace\images\207_1734507046.jpeg)";
 };
 
